c185e362e2ca81ddd95160c9777715686bea8e8d
Mubashir Ali <mubashir.imtiaz123@gmail.com>
2025-02-07 14:33:40 +0500

    Update page.tsx

--- src/app/not-authorized/page.tsx
+++ src/app/not-authorized/page.tsx
@@ -1,6 +1,19 @@
 import Navbar from '@/components/Navbar';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function NotAuthorizedPage() {
+export default async function NotAuthorizedPage() {
+  const user = await currentUser();
+  if (!user) {
+    redirect('/sign-in');
+  }
+
+  const userAccess = (user?.publicMetadata?.['product-access'] || []) as Array<{
+    title: string;
+  }>;
+  if (userAccess.some((elem) => elem.title === 'cps-publisher')) {
+    redirect('/app');
+  }
   return (
     <>
       <Navbar />
